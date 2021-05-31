167c4c9a42d687909c4081bd0801774c501cf105
MilenaChekov <60537367+MilenaChekov@users.noreply.github.com>
2021-05-31 09:41:30 +0300

    Add files via upload

--- 3.py
+++ 3.py
@@ -1,24 +1,26 @@
 import pandas as pd
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, balanced_accuracy_score
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, make_scorer
 from sklearn.model_selection import GridSearchCV, RepeatedStratifiedKFold, train_test_split
 
 df = pd.read_csv('BRCA_pam50.tsv', sep='\t', index_col=0)
 X = df.iloc[:, :-1].to_numpy()
 y = df['Subtype'].to_numpy()
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=17)
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.2, random_state=17)
 
 rfc = RandomForestClassifier(
-    random_state=17
+    random_state=17,
+    class_weight='balanced'
 )
 param_grid = {
-    'max_depth': list(range(1, 20))
+    'n_estimators': [100, 200],
+    'max_depth': list(range(1, 11)),
 }
-model = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=RepeatedStratifiedKFold(n_repeats=10))
+model = GridSearchCV(estimator=rfc, param_grid=param_grid, n_jobs=-1, scoring=make_scorer(balanced_accuracy_score), cv=RepeatedStratifiedKFold(n_repeats=10))
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
-test_BA = accuracy_score(y_test, y_pred)
+test_BA = balanced_accuracy_score(y_test, y_pred)
 print(test_BA)
 print(model.best_params_)
 
